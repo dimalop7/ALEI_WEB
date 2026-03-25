e11b72aec8f9a67f1fcedd8024cb2a83c3b9771f
piqui7777 <diegomaartinezz12@gmail.com>
2026-03-25 20:26:49 +0100

    cambio en el config

--- galeria-alei/astro.config.mjs
+++ galeria-alei/astro.config.mjs
@@ -2,8 +2,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import svelte from '@astrojs/svelte';
-
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind()]
 });
